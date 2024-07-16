d578b542c8a3d0f00dca11d8ade6312006c62e25
Dave <48639461+dagsix@users.noreply.github.com>
2024-07-16 16:50:23 -0400

    ewrwrw
    werwerwerwerew

--- js/dag22.js
+++ js/dag22.js
@@ -280,6 +280,34 @@ document.getElementById('topani').style.color = 'transparent';
 document.getElementById('topani').style.animation = 'animatedtext 3s ease-in-out infinite';
 }
 
+// date and time
+function getDateTime() {
+  var now     = new Date(); 
+  var year    = now.getFullYear();
+  var month   = now.getMonth()+1; 
+  var day     = now.getDate();
+  var hour    = now.getHours();
+  var minute  = now.getMinutes();
+  var second  = now.getSeconds(); 
+  if(month.toString().length == 1) {
+       month = '0'+month;
+  }
+  if(day.toString().length == 1) {
+       day = '0'+day;
+  }   
+  if(hour.toString().length == 1) {
+       hour = '0'+hour;
+  }
+  if(minute.toString().length == 1) {
+       minute = '0'+minute;
+  }
+  if(second.toString().length == 1) {
+       second = '0'+second;
+  }   
+  var dateTime = year+'-'+month+'-'+day+' '+hour+':'+minute+':'+second;   
+   return dateTime;
+}
+
 // init Fade
 //newFade();
 
