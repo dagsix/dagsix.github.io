02fe7535c5076d58e09c300066467a53f4d62271
Dave <48639461+dagsix@users.noreply.github.com>
2024-07-16 23:53:25 -0400

    color
    color

--- js/dag22.js
+++ js/dag22.js
@@ -256,7 +256,7 @@ const color2 = getRandomColor();
 const angle = Math.floor(Math.random() * 360);
 document.documentElement.style.background = `linear-gradient(${angle}deg, white, ${color1}, ${color2})`;
 // not multiply
-document.documentElement.style.mixBlendMode = "luminosity";
+document.documentElement.style.mixBlendMode = "color";
 }
 
 
