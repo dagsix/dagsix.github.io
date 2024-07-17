3114e6d0f7f120be3e547e3d12f532491f194478
Dave <48639461+dagsix@users.noreply.github.com>
2024-07-16 23:59:04 -0400

    color-burn
    color-burn

--- js/dag22.js
+++ js/dag22.js
@@ -255,8 +255,8 @@ const color1 = getRandomColor();
 const color2 = getRandomColor();
 const angle = Math.floor(Math.random() * 360);
 document.documentElement.style.background = `linear-gradient(${angle}deg, white, ${color1}, ${color2})`;
-// not multiply. luminosity, color, saturation hue exclusion difference
-document.documentElement.style.mixBlendMode = "difference";
+// not multiply. luminosity, color, saturation hue exclusion difference color-burn
+document.documentElement.style.mixBlendMode = "color-burn";
 }
 
 
